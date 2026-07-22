66208120129115d8d6ff282866bd9eb379fac4e4
soniasaate <soniasaate@gmail.com>
2026-07-22 22:45:38 +0330

    Add main.cpp and update CMakeLists.txt with FTXUI and all source files

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include "unmatched/TuiApp.hpp"
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <exception>
+#include <iostream>
+
+int main() {
+    try {
+        auto screen = ftxui::ScreenInteractive::Fullscreen();
+        auto app = ftxui::Make<unmatched::TuiApp>(screen);
+        screen.Loop(app);
+        return 0;
+    } catch (const std::exception& exception) {
+        std::cerr << "Fatal error: " << exception.what() << '\n';
+        return 1;
+    }
+}
