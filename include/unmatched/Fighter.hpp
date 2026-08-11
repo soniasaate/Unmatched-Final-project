8d5e427b1469f8ce4da6e9e8216c2e4c410e160e
sonia_linux <soniasaate@gmail.com>
2026-08-11 17:42:10 +0330

    fighter class

--- include/unmatched/Fighter.hpp
+++ include/unmatched/Fighter.hpp
@@ -2,7 +2,9 @@
 
 #include "unmatched/FighterDefinition.hpp"
 #include "unmatched/GameExceptions.hpp"
-#include "unmatched/GameEntity.hpp" 
+#include "unmatched/GameEntity.hpp"
+#include "unmatched/Card.hpp"
+#include <string>
 
 namespace unmatched {
 
@@ -23,6 +25,10 @@ public:
     int spaceId() const;
     bool defeated() const;
 
+    void setHealth(int health);
+    void setSpaceId(int spaceId);
+    void setDefeated(bool defeated);
+
     void placeAt(int spaceId);
     void damage(int amount);
     void heal(int amount);
@@ -30,10 +36,11 @@ public:
     void removeFromBoard();
 
     virtual std::string getSpecialAbility() const;
-
-    void setHealth(int health);
-    void setSpaceId(int spaceId);
-    void setDefeated(bool defeated);
+    virtual bool canPlayCard(const Card& card) const;
+    virtual void onCombatEffect(const Card& card, bool isAttacker,
+                                Fighter& opponent);
+    virtual void onSchemeEffect(const Card& card);
+    virtual void onTurnStart();
 
 private:
     FighterDefinition definition_;
--- src/Fighter.cpp
+++ src/Fighter.cpp
@@ -1,28 +1,83 @@
 #include "unmatched/Fighter.hpp"
 #include <algorithm>
-#include <utility>
 
 namespace unmatched {
 
 Fighter::Fighter(FighterDefinition definition)
-    : definition_(std::move(definition)),
-      health_(definition_.maxHealth()),
-      spaceId_(-1) {}
+    : GameEntity(definition.id(), definition.displayName())
+    , definition_(std::move(definition))
+    , health_(definition_.maxHealth())
+    , spaceId_(-1)
+    , defeated_(false) {
+}
 
-const FighterDefinition& Fighter::definition() const { return definition_; }
-const std::string& Fighter::id() const { return definition_.id(); }
-const std::string& Fighter::displayName() const { return definition_.displayName(); }
-Character Fighter::cardOwner() const { return definition_.cardOwner(); }
-bool Fighter::isHero() const { return definition_.isHero(); }
-int Fighter::maxHealth() const { return definition_.maxHealth(); }
-int Fighter::health() const { return health_; }
-int Fighter::move() const { return definition_.move(); }
-AttackRange Fighter::range() const { return definition_.range(); }
-int Fighter::spaceId() const { return spaceId_; }
-bool Fighter::defeated() const { return health_ <= 0; }
+const FighterDefinition& Fighter::definition() const {
+    return definition_;
+}
+
+const std::string& Fighter::id() const {
+    return definition_.id();
+}
+
+const std::string& Fighter::displayName() const {
+    return definition_.displayName();
+}
+
+Character Fighter::cardOwner() const {
+    return definition_.cardOwner();
+}
+
+bool Fighter::isHero() const {
+    return definition_.isHero();
+}
+
+int Fighter::maxHealth() const {
+    return definition_.maxHealth();
+}
+
+int Fighter::health() const {
+    return health_;
+}
+
+int Fighter::move() const {
+    return definition_.move();
+}
+
+AttackRange Fighter::range() const {
+    return definition_.range();
+}
+
+int Fighter::spaceId() const {
+    return spaceId_;
+}
+
+bool Fighter::defeated() const {
+    return defeated_;
+}
+
+void Fighter::setHealth(int health) {
+    health_ = health;
+    if (health_ <= 0) {
+        defeated_ = true;
+        spaceId_ = -1;
+    } else {
+        defeated_ = false;
+    }
+}
+
+void Fighter::setSpaceId(int spaceId) {
+    spaceId_ = spaceId;
+}
+
+void Fighter::setDefeated(bool defeated) {
+    defeated_ = defeated;
+    if (defeated_) {
+        spaceId_ = -1;
+    }
+}
 
 void Fighter::placeAt(int spaceId) {
-    if (defeated()) {
+    if (defeated_) {
         throw RuleViolation("A defeated fighter cannot be placed without revival.");
     }
     spaceId_ = spaceId;
@@ -34,6 +89,7 @@ void Fighter::damage(int amount) {
     }
     health_ = std::max(0, health_ - amount);
     if (health_ == 0) {
+        defeated_ = true;
         spaceId_ = -1;
     }
 }
@@ -42,22 +98,40 @@ void Fighter::heal(int amount) {
     if (amount < 0) {
         throw RuleViolation("Healing cannot be negative.");
     }
-    if (defeated()) {
+    if (defeated_) {
         return;
     }
     health_ = std::min(maxHealth(), health_ + amount);
 }
 
 void Fighter::reviveAt(int spaceId) {
-    health_ = std::max(1, health_);
-    if (health_ == 0) {
-        health_ = 1;
+    if (!defeated_) {
+        return;
     }
+    health_ = 1;
+    defeated_ = false;
     spaceId_ = spaceId;
 }
 
 void Fighter::removeFromBoard() {
     spaceId_ = -1;
+}
+
+std::string Fighter::getSpecialAbility() const {
+    return "None";
+}
+
+bool Fighter::canPlayCard(const Card& card) const {
+    return card.owner() == Character::Any || card.owner() == cardOwner();
+}
+
+void Fighter::onCombatEffect(const Card&, bool, Fighter&) {
+}
+
+void Fighter::onSchemeEffect(const Card&) {
+}
+
+void Fighter::onTurnStart() {
 }
 
 } // namespace unmatched
